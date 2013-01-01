b51ca88edec86430b6f69724ab3308eb83a9ecab
Mark Simpson <verdammelt@gmail.com>
2013-01-01 14:15:41 -0500

    more exercises

--- exer1.scm
+++ exer1.scm
@@ -59,3 +59,61 @@
        3))
 
   (cubert-iter 1.0 0.0))
+
+;; 1.9
+(define (plus a b)
+  (if (= a 0)
+      b
+      (inc (plus (dec a) b))))
+;; substitution for (plus 4 5)
+;; (plus 4 5)
+;; (inc (plus 3 5)
+;; (inc (inc (plus 2 5)))
+;; (inc (inc (inc (plus 1 5))))
+;; (inc (inc (inc (inc (plus 0 5)))))
+;; (inc (inc (inc (inc 5))))
+;; (inc (inc (inc 6)))
+;; (inc (inc 7))
+;; (inc 8)
+;; 9
+;; linear recursive process
+
+(define (plus a b)
+  (if (= a 0)
+      b
+      (plus (dec a) (inc b))))
+;; substitution for (plus 4 5)
+;; (plus 3 6)
+;; (plus 2 7)
+;; (plus 1 8)
+;; (plus 0 9)
+;; 9
+;; linear iterative process (also tail recursive)
+
+;; 1.10
+(define (A x y)
+  (format #t "(A ~A ~A)~%" x y)
+  (cond ((= y 0) 0)
+	((= x 0) (* 2 y))
+	((= y 1) 2)
+	(else (A (- x 1)
+		 (A x (- y 1))))))
+
+(format #t "~A~%" (A 1 10))
+(format #t "~A~%" (A 2 4))
+(format #t "~A~%" (A 3 3))
+
+(define (f n) (A 0 n))
+(define (g n) (A 1 n))
+(define (h n) (A 2 n))
+
+;; f(n) => n = 0 -> 0
+;;         n     -> 2n
+;; g(n) => n = 0 -> 0
+;;               -> 2**n
+;; h(n) => n = 0 -> 0
+;;         n     -> 2**(2**n)
+
+
+
+
