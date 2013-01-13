3c65b64cc9f39982b68cc2aba2bb3e793a49c622
Mark Simpson <verdammelt@gmail.com>
2013-01-13 16:41:51 -0500

    Updating my implementation after viewing Bill the Lizard's
    It seems that my original implemnetation was not as accurate - must
    have screwed up my definition of the function d

--- exer1.38.scm
+++ exer1.38.scm
@@ -16,10 +16,9 @@
 (define (approx-e k)
   (define (n i) 1.0)
   (define (d i) 
-    (cond ((= i 1) 1)
-	  ((= i 2) 2)
-	  ((= (remainder (- i 2) 3) 0) (* 2 (/ (- i 2) 3)))
-	  (else 1)))
+    (if (not (= 0 (remainder (+ 1 i) 3))) 
+	1
+	(* 2 (/ (+ i 1) 3))))
   (+ 2 (cont-frac n d k)))
 
 (newline)
