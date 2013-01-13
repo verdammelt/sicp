4904299f9a166c082ffa0b8722a27b9d1d719b89
Mark Simpson <verdammelt@gmail.com>
2013-01-13 16:38:23 -0500

    computing e via Euler's continued fraction method.

---
+++ exer1.38.scm
@@ -0,0 +1,32 @@
+;; from exercise 1.37
+(define (cont-frac n d k) 
+  (define (calculate n d k i)
+    (if (> i k) 1
+	(/ (n i) (+ (d i) 
+		    (calculate n d k (1+ i))))))
+  (calculate n d k 1)
+  )
+
+;; exer 1.38
+;; Euler found a N & D such that the continued fraction computes e-2
+;;
+;; Ni = 1
+;; Di = (1, 2, 1, 1, 4, 1, 1, 6, 1, 1, 8...)
+;; using cont-frac find an approximation for e
+(define (approx-e k)
+  (define (n i) 1.0)
+  (define (d i) 
+    (cond ((= i 1) 1)
+	  ((= i 2) 2)
+	  ((= (remainder (- i 2) 3) 0) (* 2 (/ (- i 2) 3)))
+	  (else 1)))
+  (+ 2 (cont-frac n d k)))
+
+(newline)
+(display 'e~)
+(display 2.71828)
+(newline)
+(display 
+ (map (lambda (k) (cons k (approx-e k)))
+      '(1 2 5 10 100 1000 10000)))
+
