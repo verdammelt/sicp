bbe44a3403ac3201788cd2c5aaccd9755f59cc76
Mark Simpson <verdammelt@gmail.com>
2013-01-21 18:56:25 -0500

    my implementation was quite complex - bill the lizard's version shows the erring of my ways.

--- chapter2/exer2.1.scm
+++ chapter2/exer2.1.scm
@@ -8,9 +8,7 @@
   (let ((g (gcd n d))
 	(abs-n (abs n))
 	(abs-d (abs d)))
-    (cond ((or (and (= n abs-n) (= d abs-d))
-	       (and (not (= n abs-n)) (not (= d abs-d))))
-	   (cons (/ abs-n g) (/ abs-d g)))
-	  (else
-	   (cons (* -1 (/ abs-n g)) (/ abs-d g))))))
+    (if (< d 0)
+	(cons (/ (* n -1) g) (/ (* d -1) g))
+	(cons (/ n g) (/ d g)))))
 
