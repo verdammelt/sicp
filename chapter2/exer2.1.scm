1b7ea68aef3a3fb6a05e34692d62e5f5ba969606
Mark Simpson <verdammelt@gmail.com>
2013-01-21 18:52:47 -0500

    Split off the definitions from the book.

--- chapter2/exer2.1.scm
+++ chapter2/exer2.1.scm
@@ -1,30 +1,4 @@
-(define (make-rat n d) 
-  (let ((g (gcd n d)))
-    (cons (/ n g) (/ d g))))
-(define (numer x) (car x))
-(define (denom x) (cdr x))
-
-(define (add-rat x y)
-  (make-rat (+ (* (numer x) (denom y))
-	       (* (numer y) (denom x)))
-	    (* (denom x) (denom y))))
-
-(define (sub-rat x y)
-  (make-rat (- (* (numer x) (denom y))
-	       (* (numer y) (denom x)))
-	    (* (denom x) (denom y))))
-
-(define (mul-rat x y)
-  (make-rat (* (numer x) (numer y))
-	    (* (denom x) (denom y))))
-
-(define (div-rat x y)
-  (make-rat (* (numer x) (denom y))
-	    (* (denom x) (numer y))))
-
-(define (equal-rat? x y)
-  (= (* (numer x) (denom y))
-     (* (numer y) (denom x))))
+(load "chapter2/rational.scm")
 
 ;; define a better version of make-rat which handles positive &
 ;; negative rational numbers. Normalize the sign so that if the
