06d306fb087dfe2fc7df9d85d480245f5a33c951
Mark Simpson <verdammelt@gmail.com>
2013-01-21 20:43:01 -0500

    whew! but i know bill the lizard's will be better.

---
+++ chapter2/exer2.5.scm
@@ -0,0 +1,32 @@
+;; show we can represent pairs of non-negative integers (a,b) as a
+;; single integer (*2^a 3^b)
+;; define cons, car and cdr
+;; (note: nni -> non-negative-integer
+(define (nni-cons a b)
+  (newline) (display "(nni-cons ") (display a) (display " ") (display b) (display ")")
+  (* (expt 2 a) (expt 3 b)))
+(define (nni-car p)
+  (let ((2part (/ p (expt 3 (nni-cdr p)))))
+    (log-n-of-x 2 2part)))
+(define (nni-cdr p)
+  (define (find-b max)
+    (cond ((= max 0) max)
+	  ((= (remainder p (expt 3 max)) 0) max)
+	  (else (find-b (- max 1)))))
+  (find-b (floor (log-n-of-x 3 p))))
+
+(define (log-n-of-x n x)
+  (/ (log x) (log n)))
+
+(define (nni-print p)
+  (newline)
+  (display "[")
+  (display p)
+  (display ";(")
+  (display (nni-car p))
+  (display " . ")
+  (display (nni-cdr p))
+  (display ")]"))
+
+(nni-print (nni-cons 2 3))
+(nni-print (nni-cons 13 23))
