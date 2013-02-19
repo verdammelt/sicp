df2e56d5d4e7435da49dbd0af57f7960efdb5209
Mark Simpson <verdammelt@gmail.com>
2013-02-18 21:28:13 -0500

    exer 2.38 - flubbed the questions - but i understand why.

---
+++ chapter2/exer2.38.scm
@@ -0,0 +1,35 @@
+(define fold-right accumulate)
+
+(define (fold-left op initial sequence)
+  (define (iter result rest)
+    (if (null? rest) result
+	(iter (op result (car rest))
+	      (cdr rest))))
+  (iter initial sequence))
+
+;; what are the values of:
+(newline)
+(display (fold-right / 1 (list 1 2 3)) )
+(newline)
+(display "I thought: 3/2")
+
+(newline)
+(display (fold-left / 1 (list 1 2 3)))
+(newline)
+(display "I thought: 3/2")		; i was wrong! 1/6
+
+(newline)
+(display (fold-right list nil (list 1 2 3)))
+(newline)
+(display "I thought: '(1 (2 (3)))")	; wrong: (1 (2 (3 ())))
+
+(newline)
+(display (fold-left list nil (list 1 2 3)))
+(newline)
+(display "I thought: (3 (2 (1)))")	; wrong: (((() 1) 2) 3)
+
+;; specify a property of op that must be true for fold-right and
+;; fold-left to have the same result for a given sequence.
+;;
+;; the op would have be such that order did not matter - such as + or *
+;; (associative?)
