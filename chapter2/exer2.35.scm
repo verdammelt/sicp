62cb6e23ad9b5b93ba907b923de9d8c5aff69b8a
Mark Simpson <verdammelt@gmail.com>
2013-02-18 19:57:15 -0500

    updated with solution by bill the lizard

--- chapter2/exer2.35.scm
+++ chapter2/exer2.35.scm
@@ -25,3 +25,18 @@
 (newline)
 (display 'count-leaves-on-test-tree===>)
 (display (count-leaves test-tree))
+
+;; bill the lizard solved it like so:
+;; (define (count-leaves t)
+;;   (accumulate +
+;; 	      0 
+;; 	      (map (lambda (x) 1) (enumerate-tree t))
+;; 	      ))
+;;
+;; while it fits exactly into the requested solution I don't like it
+;; much.
+;; on the other hand it is less complex than my solution by using
+;; simple addition of a string of 1's
+;;
+;; interesting how it is simpler but i dislike it.
+
