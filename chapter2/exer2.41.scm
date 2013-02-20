4f262071bfd4d45a0d8bbb8a34a91b4e8ce654ea
Mark Simpson <verdammelt@gmail.com>
2013-02-19 20:56:15 -0500

    a little better.

--- chapter2/exer2.41.scm
+++ chapter2/exer2.41.scm
@@ -4,14 +4,21 @@
 ;; integers i, j, k less than or equal to a given integer n that sum
 ;; to a given integer s
 (define (triplets-that-sum n s)
-  (filter (lambda (triplet)
-	    (= s (+ (car triplet)
-		    (cadr triplet)
-		    (caddr triplet))))
-	  (flatmap 
-	   (lambda (i)
-	     (flatmap (lambda (j)
-			(map (lambda (k) (list i j k))
-			     (enumerate-interval 1 (- j 1))))
-		      (enumerate-interval 1 (- i 1))))
-	   (enumerate-interval 1 n))))
+  (define (triplet-sums-to-s? triplet)
+    (= s (+ (car triplet) (cadr triplet) (caddr triplet))))
+
+  (define (unique-pairs n)
+    (flatmap (lambda (i)
+	       (map (lambda (j) (list i j))
+		    (enumerate-interval 1 (- i 1))))
+	     (enumerate-interval 1 n)))
+
+  (define (unique-triplets n)
+    (flatmap (lambda (i) 
+	       (flatmap (lambda (j)
+			  (map (lambda (k) (list i j k))
+			       (enumerate-interval 1 (- j 1))))
+			      (enumerate-interval 1 (- i 1))))
+	     (enumerate-interval 1 n)))
+
+  (filter triplet-sums-to-s? (unique-triplets n)))
