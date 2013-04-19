dfc3bcaf24f3b0068b3b991f55a48e51a1f689f2
Mark Simpson <verdammelt@gmail.com>
2013-04-18 21:26:55 -0400

    Had forgotten to simplifie the exponent expressions.
    Also had screwed up make-product a bit.

--- chapter2/exer2.56.scm
+++ chapter2/exer2.56.scm
@@ -8,4 +8,10 @@
 (equal? (deriv '(** x 3) 'x)
 	'(* 3 (** x 2)))
 
+(equal? (deriv '(* a (** x 2)) 'x)
+	'(* a (* 2 x)))
+
+(equal? (deriv '(+ (+ (* a (** x 2)) (* b x)) c) 'x)
+	'(+ (* a (* 2 x)) b))
+
 
