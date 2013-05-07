73bc1f553b9ef03e402483fd918cdf814009c8bf
Mark Simpson <verdammelt@gmail.com>
2013-05-07 15:48:13 -0400

    exercise 2.64

--- chapter2/set-as-binary-tree.scm
+++ chapter2/set-as-binary-tree.scm
@@ -73,3 +73,46 @@
 
 ;; b) do they have the same order of growth?
 ;; The second appears to be tail recursive.
+
+;; exercise 2.64
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+	(let ((left-result (partial-tree elts left-size)))
+	  (let ((left-tree (car left-result))
+		(non-left-elts (cdr left-result))
+		(right-size (- n (+ left-size 1))))
+	    (let ((this-entry (car non-left-elts))
+		  (right-result (partial-tree (cdr non-left-elts)
+					      right-size)))
+	      (let ((right-tree (car right-result))
+		    (remaining-elts (cdr right-result)))
+		(cons (make-tree this-entry left-tree right-tree)
+		      remaining-elts))))))))
+
+;; a. Write a short paragraph explaining as clearly as you can how
+;; partial-tree works. Draw the tree produced by list->tree for the
+;; list (1 3 5 7 9 11).
+
+;; It splits the list in 'half' around a central element (the left
+;; side will always be shorter. Then creates a tree from that,
+;; recursing down the left and right sides.
+
+;; a 'drawing':
+;;   5
+;;  / \
+;; 1   \
+;;  \   \
+;;   3   9
+;;      / \
+;;     7   11
+
+
+;; b. What is the order of growth in the number of steps required by
+;; list->tree to convert a list of n elements? 
+;; Order of logn?
+
+
