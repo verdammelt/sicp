e8b6f0719dd2e2edce1adf89404ce3851e531ed4
Mark Simpson <verdammelt@gmail.com>
2013-05-07 15:34:45 -0400

    quick answers to 2.63

---
+++ chapter2/set-as-binary-tree.scm
@@ -0,0 +1,75 @@
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+	((= x (entry set)) true)
+	((< x (entry set))
+	 (element-of-set? x (left-branch set)))
+	((> x (entry set))
+	 (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x '() '()))
+	((= x (entry set)) set)
+	((< x (entry set))
+	 (make-tree (entry set)
+		    (adjoin-set x (left-branch set))
+		    (right-branch set)))
+	((> x (entry set))
+	 (make-tree (entry set)
+		    (left-branch set)
+		    (adjoin-set x (right-branch set))))))
+
+;; exercise 2.63:
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+	      (cons (entry tree)
+		    (tree->list-1 (right-branch tree))))))
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+	result-list
+	(copy-to-list (left-branch tree)
+		      (cons (entry tree)
+			    (copy-to-list (right-branch tree)
+					  result-list)))))
+  (copy-to-list tree '()))
+
+;; example trees
+(define tree-1 
+  (make-tree 7 
+	     (make-tree 3 
+			(make-tree 1 '() '()) 
+			(make-tree 5 '() '())) 
+	     (make-tree 9 
+			'() 
+			(make-tree 11 '() '()))))
+(define tree-2 
+  (make-tree 3 
+	     (make-tree 1 '() '()) 
+	     (make-tree 7 
+			(make-tree 5 '() '()) 
+			(make-tree 9 '() 
+				   (make-tree 11 '() '())))))
+(define tree-3 
+  (make-tree 5 
+	     (make-tree 3 
+			(make-tree 1 '() '()) 
+			'())
+	     (make-tree 9
+			(make-tree 7 '() '())
+			(make-tree 11 '() '()))))
+
+;; a) do these produce the same result for the same inputs? if not how
+;; are they different? what do they do for the trees listed in figure
+;; 2.16? 
+;; I think they produce the same outputs with the same inputs.
+
+;; b) do they have the same order of growth?
+;; The second appears to be tail recursive.
