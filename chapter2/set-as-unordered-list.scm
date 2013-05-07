9650fde9002b0684d459d461d19d12ab1abe32c0
Mark Simpson <verdammelt@gmail.com>
2013-05-07 14:50:36 -0400

    tweaking implementation after seeing Bill-the-Lizard's

--- chapter2/set-as-unordered-list.scm
+++ chapter2/set-as-unordered-list.scm
@@ -20,7 +20,6 @@
 (define (union-set set1 set2)
   (cond ((null? set1) set2)
 	((null? set2) set1)
-	((not (element-of-set? (car set1) set2))
-	 (union-set (cdr set1) (cons (car set1) set2)))
-	(else (union-set (cdr set1) set2))))
+	((element-of-set? (car set1) set2) (union-set (cdr set1) set2))
+	(else (cons (car set1) (union-set (cdr set1) set2)))))
 
