eda39510f9c8933ce91a9d72c725721ab4afaac2
Mark Simpson <verdammelt@gmail.com>
2013-06-09 16:30:03 -0400

    exercise 3.3

--- chapter3/exer3.1.scm
+++ chapter3/exer3.1.scm
@@ -12,5 +12,25 @@
 	    ((eq? arg 'reset-count) (begin (set! num-calls 0) num-calls))
 	    (else (begin (set! num-calls (1+ num-calls)) (fn arg)))))))
 
+;; exercise 3.3
+;; add a password to the make-account code shown in the book
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+	(begin (set! balance (- balance amount))
+	       balance)
+	"Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch attempted-password m)
+    (if (eq? attempted-password password)
+	(cond ((eq? m 'withdraw) withdraw)
+	      ((eq? m 'deposit) deposit)
+	      (else (error "Unknown request -- MAKE-ACCOUNT"
+			   m)))
+	(error "Incorrect password")))
+  dispatch)
+
 
 
