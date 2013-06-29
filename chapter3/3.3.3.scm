05522092d0083232d45c05e853b248961e147394
Mark Simpson <verdammelt@gmail.com>
2013-06-29 14:06:27 -0400

    cleaned up my 'solution' to 3.25 by removing the code.

--- chapter3/3.3.3.scm
+++ chapter3/3.3.3.scm
@@ -46,30 +46,15 @@
 
 ;; exercise 3.25
 ;; generalize to a n-table - lookup and insert! take a list of keys
-(define (make-n-table same-key?)
-  (let ((local-table (list '*table*)))
-    (define (assoc key records)
-      (cond ((null? records) false)
-	    ((same-key? key (caar records)) (car records))
-	    (else (assoc key (cdr records)))))
-    (define (print) (display local-table))
-    (define (lookup key)
-      (let ((record (assoc key (cdr local-table))))
-	(if record (cdr record) false)))
-    (define (insert! key value)
-      (let ((record (assoc key (cdr local-table))))
-	(if record
-	    (set-cdr! record value)
-	    (set-cdr! local-table (cons (cons key value) 
-					(cdr local-table))))))
-    (define (dispatch m) 
-      (cond ((eq? m 'lookup) lookup)
-	    ((eq? m 'insert!) insert!)
-	    ((eq? m 'print) print)
-	    (else (error "Unknown operation -- table " m))))
-    dispatch))
+;; nothing to do here! just use equal? and the lists are keys.
+;; an alternative is to write a bunch of code which can recursively
+;; insert/lookup until it gets to a single key which is a
+;; insert-1/lookup-1 type case
+;; I choose no more code. - in fact i am removing this code here!
+(define (make-n-table)
+  (make-table equal?))
 
-(define nt (make-n-table equal?))
+(define nt (make-n-table))
 ((nt 'insert!) '(a b c) 1)
 ((nt 'insert!) '(a b z) 2)
 ((nt 'insert!) '(x y z) 3)
@@ -78,3 +63,4 @@
 ((nt 'print)) (newline)
 (display ((nt 'lookup) '(x y))) (newline)
 (display ((nt 'lookup) (list 'x 'y))) (newline)
+
