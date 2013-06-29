b53ed55a8ea572b5f51c5a801bd48cd5d7a8fe55
Mark Simpson <verdammelt@gmail.com>
2013-06-29 14:32:43 -0400

    defining an alternative or-gate

--- chapter3/3.3.4.scm
+++ chapter3/3.3.4.scm
@@ -67,4 +67,24 @@
   (add-action! a2 or-gate-procedure)
   'ok)
 
+;; exercise 3.29 - define or-gate as with and-gate and inverters
+;; or-gate = and-gate with negated inputs and outputs
+(define (other-or-gate a1 a2 output)
+  (let ((a1-inv (make-wire))
+	(a2-inv (make-wire))
+	(and-output (make-wire)))
+    (inverter a1 a1-inv)
+    (inverter a2 a2-inv)
+    (and-gate a1-inv a2-inv and-output)
+    (inverter and-output output)))
+
+;; the delay of this other-or-gate would be: 
+;; (+ and-gate-delay (* 2 inverter-delay)) 
+;;    - if a1 & a2 were set at the same time
+;; (+ and-gate-delay (* 3 inverter-delay))
+;;    - if a1 & a2 are signaled at different times
+
+
+
+
 
