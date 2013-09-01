9aba8f24f4abd6ba68e8b637bbab31919f26f8dd
Mark Simpson <verdammelt@gmail.com>
2013-08-31 23:00:01 -0400

    exercise: 3.35 - working definition of squarer constraint

--- chapter3/3.3.5.scm
+++ chapter3/3.3.5.scm
@@ -25,6 +25,40 @@
 ;; b as multiplier needs two of its three connectors to have values to 
 ;; compute the third.
 
+;; exercise 3.35
+;; Ben Bitdiddle tells Louis that squarer needs to be a primitive 
+;; constraint. Finish the implementation:
+(define (squarer a b)
+  (define (process-new-value)
+    (if (has-value? b)
+	(if (< (get-value b) 0)
+	    (error "square less than 0 -- SQUARER" (get-value b))
+	    ;; alternative 1
+	    (set-value! a 
+			(sqrt (get-value b))
+			me))
+	;; alternative 2
+	(set-value! b 
+		    (square (get-value a))
+		    me)))
+  (define (process-forget-value)
+    ;; body 1
+    (forget-value! a me)
+    (forget-value! b me)
+    (process-new-value))
+  (define (me request)
+    ;; body 2
+    (cond ((eq? request 'I-have-a-value)
+	   (process-new-value))
+	  ((eq? request 'I-lost-my-value)
+	   (process-forget-value))
+	  (else
+	   (error "Unknown request -- SQUARER" request))))
+  ;; rest of definition
+  (connect a me)
+  (connect b me)
+  me)
+
 
 
 
