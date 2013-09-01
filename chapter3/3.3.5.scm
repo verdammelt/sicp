ab52bfebcc101ab55247835ddb7ae20b91334594
Mark Simpson <verdammelt@gmail.com>
2013-08-31 22:51:30 -0400

    exercise 3.34 answer

--- chapter3/3.3.5.scm
+++ chapter3/3.3.5.scm
@@ -13,3 +13,18 @@
     (constant 0.5 half)
     'ok))
 
+;; exercise 3.34
+
+;; Louis Reasoner wants to build a squarer constraint
+;; he defines it like:
+(define (squarer a b)
+  (muliplier a a b))
+;; it has a serious flaw. Explain.
+;; mjs: it can only work in one direction. Setting a will cause b 
+;; to be computed - but it cannot compute the value for a based upon
+;; b as multiplier needs two of its three connectors to have values to 
+;; compute the third.
+
+
+
+
