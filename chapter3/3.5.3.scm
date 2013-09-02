9d779fb07373d113bb1f79bf32092928c65a7e05
Mark Simpson <verdammelt@gmail.com>
2013-09-02 17:56:08 -0400

    skipping a few exercises

--- chapter3/3.5.3.scm
+++ chapter3/3.5.3.scm
@@ -70,5 +70,20 @@
 ;; was pretty amazing.
 
 
+;; Infinite streams of pairs
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+		   (interleave s2 (stream-cdr s1)))))
 
+(define (pairs s t)
+  (cons-stream
+   (list (stream-car s) (stream-car t))
+   (interleave
+    (stream-map (lambda (x) (list (stream-car s) x))
+		(stream-cdr t))
+    (pairs (stream-cdr s) (stream-cdr t)))))
+
+;; exercise 3.66-3.68 - skipped
 
