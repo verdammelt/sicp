f4875603623c3fef85d1cc6e0f6c83cb5dc4f521
Mark Simpson <verdammelt@gmail.com>
2013-09-02 18:37:18 -0400

    exercise 3.69 - ultimately took eli's version
    Mine /seemed/ to generate all the right values - but finding (3,4,5)
    was hanging (i.e. taking longer than my patience (several 10s of
    seconds)).
    
    Eli's version however got to (3,4,5) pretty quickly.

--- chapter3/3.5.3.scm
+++ chapter3/3.5.3.scm
@@ -60,6 +60,10 @@
 (define (ln-2-summands n)
   (cons-stream (/ 1.0 n)
 	       (stream-map - (ln-2-summands (1+ n)))))
+(define (partial-sums stream)
+  (cons-stream (stream-car stream)
+	       (add-streams (stream-cdr stream)
+			    (partial-sums stream))))
 (define ln-2-stream
   (partial-sums (ln-2-summands 1)))
 
@@ -86,4 +90,39 @@
     (pairs (stream-cdr s) (stream-cdr t)))))
 
 ;; exercise 3.66-3.68 - skipped
+
+;; exercise 3.69
+;; write a triples function (like pairs) then use it to find 
+;; pythagorean triples
+(define (add-streams s1 s2) (stream-map + s1 s2))
+(define ones (cons-stream 1 ones))
+(define integers (cons-stream 1 (add-streams ones integers)))
+(define (triples s1 s2 s3)
+  (cons-stream 
+    (list
+      (stream-car s1)
+      (stream-car s2)
+      (stream-car s3))
+    (interleave
+      (stream-map
+        (lambda (x) (append (list (stream-car s1)) x))
+        (stream-cdr (pairs s2 s3)))
+      (triples 
+        (stream-cdr s1)
+        (stream-cdr s2)
+        (stream-cdr s3)))))
+
+(define pythagorean-triples
+  (stream-filter (lambda (x)
+		   (let ((i (car x))
+			 (j (cadr x))
+			 (k (caddr x)))
+		     (if (and (= i 3)
+			      (= j 4)
+			      (= k 5))
+			 (display 'FOUND-ONE))
+		     (= (+ (square i)
+			   (square j))
+			(square k))))
+		 (triples integers integers integers)))
 
