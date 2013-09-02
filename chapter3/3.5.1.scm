ab26dfbf8d05e4390a7b537301bae41ec77d627b
Mark Simpson <verdammelt@gmail.com>
2013-09-02 14:41:30 -0400

    foo

--- chapter3/3.5.1.scm
+++ chapter3/3.5.1.scm
@@ -1,3 +1,23 @@
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+       low
+       (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+	     (stream-for-each proc (stream-cdr s)))))
+
+(define (display-line x)
+  (newline)
+  (display x))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
 (define integers
   (cons-stream 1 (stream-map 1+ integers)))
 
@@ -12,3 +32,29 @@
 	      (cons proc (map stream-cdr argstreams))))))
 
 (define squares (stream-map-2 * integers integers))
+
+;; 3.51
+(define (show x)
+  (display-line x)
+  x)
+
+(define x (stream-map show (stream-enumerate-interval 0 10)))
+
+;; (stream-ref x 10)
+
+;; (stream-ref x 7)
+
+;; 3.52
+(define sum 0)
+(define (accum x)
+  (set! sum (+ x sum))
+  sum)
+
+(define seq (stream-map accum (stream-enumerate-interval 1 20)))
+(define y (stream-filter even? seq))
+(define z (stream-filter (lambda (x) (= (remainder x 5) 0)) seq))
+
+;(stream-ref y 7)
+
+;(display-stream z)
+
