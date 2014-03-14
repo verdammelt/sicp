e109ec52830006ce4ab9152ed0985a1f72325c95
Mark Simpson <verdammelt@gmail.com>
2014-03-14 17:09:15 -0400

    4.40 - improved version found on net.

--- chapter4/4.3.1-4.40.scm
+++ chapter4/4.3.1-4.40.scm
@@ -47,3 +47,22 @@
 		  (list 'fletcher fletcher)
 		  (list 'miller miller)
 		  (list 'smith smith))))))))
+
+;; an improved version found on the net:
+;; http://community.schemewiki.org/?sicp-ex-4.40
+;; puts cooper and miller first since they are the most restrictive.
+(define (mutiple-dwelling) 
+   (let ((cooper (amb 2 3 4 5)) 
+         (miller (amb 3 4 5)))  
+     (require (> miller cooper)) 
+     (let ((fletcher (amb 2 3 4))) 
+       (require (not (= (abs (- fletcher cooper)) 1))) 
+       (let ((smith (amb 1 2 3 4 5))) 
+         (require (not (= (abs (- smith fletcher)) 1))) 
+         (let ((baker (amb 1 2 3 4))) 
+           (require (distinct? (list baker cooper fletcher miller smith))) 
+           (list (list 'baker baker) 
+                 (list 'cooper cooper) 
+                 (list 'fletcher fletcher) 
+                 (list 'miller miller) 
+                 (list 'smith smith)))))))
