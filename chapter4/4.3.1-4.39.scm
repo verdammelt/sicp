a845b31cc25c093ceacc3ea845f05357c406a68b
Mark Simpson <verdammelt@gmail.com>
2014-03-14 16:42:36 -0400

    4.39

---
+++ chapter4/4.3.1-4.39.scm
@@ -0,0 +1,8 @@
+;; Q: does the ordering of restrictions change the answer?
+
+;; A: no.
+
+;; Q:does the ordering of restrictions change the time required?
+
+;; A: yes because different orderings will cause more or less invalid
+;; choices to be tried.
