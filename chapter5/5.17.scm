9c7b27704ad9b45360a7efccccbdbaac952d6e98
Mark Simpson <verdammelt@gmail.com>
2014-06-12 20:49:43 -0400

    5.17 - displaying label during tracing.
    Got stumped on this - looked on-line and saw the same idea (but in
    theory - working). Committed is a hack of it - not tested.

---
+++ chapter5/5.17.scm
@@ -0,0 +1,100 @@
+;; Extend the instruction tracing of exercise 5.16 so that before
+;; printing an instruction, the simulator prints any labels that
+;; immediately precede that instruction in the controller sequence. Be
+;; careful to do this in a way that does not interfere with
+;; instruction counting (exercise 5.15). You will have to make the
+;; simulator retain the necessary label information.
+
+(define (make-new-machine)
+  (let ((pc (make-register 'pc))
+        (flag (make-register 'flag))
+        (stack (make-stack))
+        (the-instruction-sequence '())
+	(trace-on #f)
+	(instruction-count 0))
+    (let ((the-ops
+           (list (list 'initialize-stack
+                       (lambda () (stack 'initialize)))
+                 ;;**next for monitored stack (as in section 5.2.4)
+                 ;;  -- comment out if not wanted
+                 (list 'print-stack-statistics
+                       (lambda () (stack 'print-statistics)))))
+          (register-table
+           (list (list 'pc pc) (list 'flag flag))))
+      (define (allocate-register name)
+        (if (assoc name register-table)
+            (error "Multiply defined register: " name)
+            (set! register-table
+                  (cons (list name (make-register name))
+                        register-table)))
+        'register-allocated)
+      (define (lookup-register name)
+        (let ((val (assoc name register-table)))
+          (if val
+              (cadr val)
+              (error "Unknown register:" name))))
+      (define (execute)
+        (let ((insts (get-contents pc)))
+          (if (null? insts)
+              'done
+              (begin
+		(if trace-on
+		    (begin
+		      (display instruction-count)
+		      (display "> [")
+		      (if (instruction-label (car insts))
+			  (display (instruction-label (car insts))))
+		      (display "] ")
+		      (display (instructino-text (car insts)))
+		      (newline)))
+                ((instruction-execution-proc (car insts)))
+		(set! instruction-count (1+ instruction-count))
+                (execute)))))
+      (define (dispatch message)
+        (cond ((eq? message 'start)
+	       (set! instruction-count 0)
+               (set-contents! pc the-instruction-sequence)
+               (execute))
+              ((eq? message 'install-instruction-sequence)
+               (lambda (seq) (set! the-instruction-sequence seq)))
+              ((eq? message 'allocate-register) allocate-register)
+              ((eq? message 'get-register) lookup-register)
+              ((eq? message 'install-operations)
+               (lambda (ops) (set! the-ops (append the-ops ops))))
+              ((eq? message 'stack) stack)
+              ((eq? message 'operations) the-ops)
+	      ((eq? message 'trace-on) (set! trace-on #t))
+	      ((eq? message 'trace-off) (set! trace-on #f))
+              (else (error "Unknown request -- MACHINE" message))))
+      dispatch)))
+
+;; instructions will have an additional field for labels
+(define (make-instruction text) 
+  (list text '() '())) 
+(define (make-instruction-with-label text label) 
+  (list text label '())) 
+(define (instruction-text inst) 
+  (car inst)) 
+(define (instruction-label inst) 
+  (cadr inst)) 
+(define (instruction-execution-proc inst) 
+  (caddr inst)) 
+(define (set-instruction-execution-proc! inst proc) 
+  (set-car! (cddr inst) proc)) 
+  
+(define (extract-labels text receive)
+  (if (null? text)
+      (receive '() '())
+      (extract-labels (cdr text)
+       (lambda (insts labels)
+         (let ((next-inst (car text)))
+           (if (symbol? next-inst)
+	       (let ((first-inst (make-instructions-with-label (instruction-text (car insts))
+								next-inst))))
+               (receive (cons first-inst (cdr insts))
+                        (cons (make-label-entry next-inst
+                                                insts)
+                              labels))
+               (receive (cons (make-instruction next-inst)
+                              insts)
+                        labels)))))))
