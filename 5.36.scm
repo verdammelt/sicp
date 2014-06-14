8660dac73a783692fecb26164129f70d190fec88
Mark Simpson <verdammelt@gmail.com>
2014-06-14 17:01:58 -0400

    5.36 - updating after reviewing answer on http://community.schemewiki.org/?sicp-ex-5.36.

--- 5.36.scm
+++ 5.36.scm
@@ -9,3 +9,8 @@
 ;; since it only needs to start with a list of the right-most argument
 ;; and cons on the next left argument. Consing to the end of a list is
 ;; less efficient.
+
+;; UPDATE after reading http://community.schemewiki.org/?sicp-ex-5.36.
+;; that answer points out that the creating arglist could be reversed
+;; with an instruction. So instead of doing the work during
+;; compilation the work would be done during execution.
